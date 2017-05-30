e44e6fb3fcd95fe58f55ebd31ad6c3d8ee10ff59
Maksim Kotlyar <kotlyar.maksim@gmail.com>
2017-05-30 16:39:36 +0300

    upd

---
+++ apps/jm/etc/bundles.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'Symfony\Bundle\FrameworkBundle\FrameworkBundle' => ['all' => true],
+    'Makasim\Yadm\Bundle\YadmBundle' => ['all' => true],
+    'Enqueue\Bundle\EnqueueBundle' => ['all' => true],
+];
