e1aca8f88363b9ba4e28fb38805420f61f32a68a
Maksim Kotlyar <kotlyar.maksim@gmail.com>
2017-05-30 16:39:48 +0300

    upd

--- apps/job_manager/etc/bundles.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-return [
-    'Symfony\Bundle\FrameworkBundle\FrameworkBundle' => ['all' => true],
-    'Makasim\Yadm\Bundle\MakasimBundle' => ['all' => true],
-    'Enqueue\Bundle\EnqueueBundle' => ['all' => true],
-];
